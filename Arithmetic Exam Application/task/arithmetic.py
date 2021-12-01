0f9f980217258c36afca72c6b6db16f977192fad
rolmos14 <olmos.rafa@gmail.com>
2021-12-01 23:05:17 +0100

    Stage 1/4. Mini-calculator

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -1,1 +1,11 @@
-# write your code here
+class ArithmeticExam:
+
+    def __init__(self):
+        pass
+
+    def calculate(self, operation):
+        return eval(operation)
+
+operation = input()
+exam = ArithmeticExam()
+print(exam.calculate(operation))
