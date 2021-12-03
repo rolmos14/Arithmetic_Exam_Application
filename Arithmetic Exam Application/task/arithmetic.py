a385e0a7fa1aa5a29c0c711afa63cf4277d09727
rolmos14 <olmos.rafa@gmail.com>
2021-12-03 21:40:55 +0100

    Stage 2/4. Added method task which generates a random operation for the user to answer.

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -1,11 +1,25 @@
+import random
+
+
 class ArithmeticExam:
 
     def __init__(self):
         pass
 
+    def task(self):
+        x = random.randint(2, 9)
+        y = random.randint(2, 9)
+        operator = random.choice(["+", "-", "*"])
+        operation = f'{x} {operator} {y}'
+        answer = int(input(f'{operation}\n'))
+        if answer == self.calculate(operation):
+            print("Right!")
+        else:
+            print("Wrong!")
+
     def calculate(self, operation):
         return eval(operation)
 
-operation = input()
+
 exam = ArithmeticExam()
-print(exam.calculate(operation))
+exam.task()
